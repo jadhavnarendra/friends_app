7b7089f5f433e2cb48384781f20f78866a32191f
narendra <narendrajadhav0707@gmail.com>
2022-02-01 19:42:11 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: [:github, :google_oauth2]
+
+         has_many :friends
+
+         def self.from_omniauth(access_token)
+          data = access_token.info
+          user = User.where(email: data['email']).first
+                
+          unless user
+              user = User.create(
+                 email: data['email'],
+                 password: Devise.friendly_token[0,20]
+              )
+          end
+          user
+        end
+end
